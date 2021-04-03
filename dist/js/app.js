1e5a4360610aac8bf90c28d14068f1e7bb76c647
alextr999 <tronenko.aleksandr@gmail.com>
2021-04-03 21:47:31 +0300

    ready block find-aut

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
     $('.jsPhone').inputmask("+38(099)999-99-99");
-});
-$(document).ready(function() {
     $('.jsYear').inputmask("9999");
+
+    let mobiMenu = false
+    
+    $('.jsMobMenu').on('click', function(e) {
+        e.preventDefault();
+        if (mobiMenu) {
+            $('.mobile-menu').removeClass('active');
+            mobiMenu = false
+        } else {
+            $('.mobile-menu').addClass('active');
+            mobiMenu = true
+        }
+    })
 });
