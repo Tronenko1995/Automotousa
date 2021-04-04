a381054e70e60c4b619cc5fd96b215a1fcf09bf1
alextr999 <tronenko.aleksandr@gmail.com>
2021-04-05 01:24:45 +0300

    ready all

--- dist/js/app.js
+++ dist/js/app.js
@@ -198,7 +198,8 @@ $(document).ready(function() {
         currentHotRender = 0,
         tempHot = 0,
         currentAuctionRender = 0,
-        tempAuction = 0;
+        tempAuction = 0,
+        modal = false;
     
     $('.jsMobMenu').on('click', function(e) {
         e.preventDefault();
@@ -237,7 +238,7 @@ $(document).ready(function() {
                 <p class="hot-offer__item-text">${item.engine}</p>
             </div>
             <p class="hot-offer__item-text">${item.description}<br>${item.descriptionNext}</p>
-            <button class="hot-offer__item-button callback__button">Обратный звонок</button>
+            <button class="hot-offer__item-button callback__button jsCallback" data-product="${item.name}">Обратный звонок</button>
         </li>`)
     }
 
@@ -262,7 +263,7 @@ $(document).ready(function() {
                 <p class="join-auction__item-text">Двигатель</p>
                 <p class="join-auction__item-text">${item.engine}</p>
             </div>
-            <button class="join-auction__item-button callback__button">Обратный звонок</button>
+            <button class="join-auction__item-button callback__button jsCallback data-product="${item.name}">Обратный звонок</button>
         </li>`)
     }
 
@@ -336,7 +337,7 @@ $(document).ready(function() {
                 <p class="hot-offer__item-text">${item.engine}</p>
             </div>
             <p class="hot-offer__item-text">${item.description}<br>${item.descriptionNext}</p>
-            <button class="hot-offer__item-button callback__button">Обратный звонок</button>`).addClass('hot-offer__item svg-fire')
+            <button class="hot-offer__item-button callback__button jsCallback" data-product="${item.name}">Обратный звонок</button>`).addClass('hot-offer__item svg-fire')
         $(container).append(newItem)
             tempHot = i;
         }
@@ -366,5 +367,55 @@ $(document).ready(function() {
     $('.jsMoreAuction').on('click', function() {
         loadMoreAuctionAuto();
     })
+    $('.modal__input').on('input', function() {
+        if ($(this).val() !== '') {
+            $(this).closest('.modal__input-wrap').addClass('hide-after');
+        } else {
+            $(this).closest('.modal__input-wrap').removeClass('hide-after');
+        }
+    })
+    $('.modal__textarea').on('input', function() {
+        if ($(this).val() !== '') {
+            $(this).closest('.modal__textarea-wrap').addClass('hide-after');
+        } else {
+            $(this).closest('.modal__textarea-wrap').removeClass('hide-after');
+        }
+    })
 
+    $(document).on('click','.jsCallback', function(e) {
+        if ($(this).data('product')) {
+            $('.modal').find('[name="product"]').val($(this).data('product'))
+        } else {
+            $('.modal').find('[name="product"]').val('')
+        }
+        e.preventDefault();
+        if (modal) {
+            $('.modal').addClass('hide');
+            $('body').css('overflow', 'unset');
+            modal = false
+        } else {
+            $('.modal').removeClass('hide');
+            $('body').css('overflow', 'hidden');
+            modal = true
+        }
+    })
+
+    $('.modal__form').on('submit', function(e) {
+        e.preventDefault();
+        const data = $(this).serialize()
+        $.ajax({
+            method: 'POST',
+            url: 'php/mail.php',
+            data: data })
+            .done(function() {
+                console.log('успех')
+                $('.jsCallback').click();
+                alert('Спасибо, сообщение успешно отправлено, мы скоро с вами свяжемся');
+            })
+            .fail(function() {
+                console.log('ошибка')
+                alert('Ошибка');
+                $('.jsCallback').click();
+            })
+    })
 });
