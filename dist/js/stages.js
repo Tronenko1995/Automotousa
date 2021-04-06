9a2ade580dae600b8e2b24bcf214f6bf6f1b7b96
alextr999 <tronenko.aleksandr@gmail.com>
2021-04-07 01:41:08 +0300

    developen page stages

---
+++ dist/js/stages.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    // const slider = new Swiper('.jsSlider', {
+    //     loop: true,
+    //     effect: 'fade',
+    //     slidesPerView: 3,
+    //     spaceBetween: 30,
+    //     autoplay: {
+    //         delay: 3000,
+    //         disableOnInteraction: false,
+    //     },
+    //     pagination: {
+    //         el: '.swiper-pagination',
+    //         clickable: true
+    //     }
+    // });
+
+
+});
